4838e4f5404428cf4836ae6abbb64eacbfeb6399
Marc <lekazki90@gmail.com>
2013-04-22 19:38:28 +0200

    empezando a crear el proyecto

---
+++ src/paquete1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package paquete1;
+
+/**
+ * @author Marc
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
